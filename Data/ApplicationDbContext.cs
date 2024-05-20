49ccbc50363b0833e15695976619e7fc9048ee9b
Onestyle7 <klusbartek8@gmail.com>
2024-05-20 18:30:52 +0200

    Added initial migration Added DocumentsController with views

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DocsApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocsApp.Data
@@ -9,5 +10,6 @@ namespace DocsApp.Data
             : base(options)
         {
         }
+        public DbSet<Document> Docs { get; set; }
     }
 }
